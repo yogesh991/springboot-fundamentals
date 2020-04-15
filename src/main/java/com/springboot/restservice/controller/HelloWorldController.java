93d5db942220a19b780eb17074b20582ec6feda5
Yogesh Deshmukh <sampoornesh@Yogeshs-MacBook-Air.local>
2020-04-15 22:22:33 +0530

    Added a Test Controller and Test Cases for the same

---
+++ src/main/java/com/springboot/restservice/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.springboot.restservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloWorldController {
+
+    @GetMapping("/hello/{name}")
+    public ResponseEntity<String> testMethod(@PathVariable("name")String name){
+        return ResponseEntity.ok("Hello "+name);
+    }
+
+}
---
+++ src/test/java/com/springboot/restservice/controller/HelloWorldControllerTest.java
@@ -0,0 +1,36 @@
+package com.springboot.restservice.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloWorldController.class)
+public class HelloWorldControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHelloControllerBasic() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/api/hello/Yogesh")
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello Yogesh"))
+                .andReturn();
+
+        assertEquals("Hello Yogesh", result.getResponse().getContentAsString());
+    }
+}
