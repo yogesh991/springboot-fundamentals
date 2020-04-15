f049c98607685c8e79f06f6a0607468c43c1bade
Yogesh Deshmukh <sampoornesh@Yogeshs-MacBook-Air.local>
2020-04-15 10:53:26 +0530

    First Commit

---
+++ src/main/java/com/springboot/restservice/SpringbootBuildingblockApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingblockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingblockApplication.class, args);
+	}
+
+}
