ab9f4ef4dede90b4d374fbf31ff720c0370ad996
Yogesh Deshmukh <sampoornesh@Yogeshs-MacBook-Air.local>
2020-04-16 00:45:17 +0530

    added User bean and returned it from Rest method

--- src/main/java/com/springboot/restservice/controller/HelloWorldController.java
+++ src/main/java/com/springboot/restservice/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.springboot.restservice.controller;
 
+import com.springboot.restservice.model.UserDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,11 @@ public class HelloWorldController {
     @GetMapping("/hello/{name}")
     public ResponseEntity<String> testMethod(@PathVariable("name")String name){
         return ResponseEntity.ok("Hello "+name);
+    }
+
+    @GetMapping("/")
+    public UserDetail getUserDetails(){
+        return null;
     }
 
 }
---
+++ src/main/java/com/springboot/restservice/model/UserDetail.java
@@ -0,0 +1,53 @@
+package com.springboot.restservice.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDetail {
+
+    private String firstName;
+    private String lastName;
+    private String city;
+
+    public UserDetail() {
+    }
+
+    public UserDetail(String firstName, String lastName, String city) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDetail{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
