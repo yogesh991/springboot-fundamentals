feed8faf49b10f6e6fd632a80c6590f4e06a8204
Yogesh Deshmukh <sampoornesh@Yogeshs-MacBook-Air.local>
2020-04-19 09:12:14 +0530

    updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,5 @@
-insert into USER (ID, EMAIL, FIRST_NAME, LAST_NAME, ROLE, SSN, USER_NAME) values(101, 'abc@gmail.com', 'LL', 'KK', 'Associate', 'ssn10', 'R670516');
-insert into USER (ID, EMAIL, FIRST_NAME, LAST_NAME, ROLE, SSN, USER_NAME) values(102, 'fcd@gmail.com', 'KK', 'KK', 'VP', 'ssn101', 'R670510');
-insert into USER (ID, EMAIL, FIRST_NAME, LAST_NAME, ROLE, SSN, USER_NAME) values(103, 'sku@gmail.com', 'DD', 'KK', 'Junior Associate', 'ssn110', 'R657016');
-insert into USER (ID, EMAIL, FIRST_NAME, LAST_NAME, ROLE, SSN, USER_NAME) values(104, 'fff@gmail.com', 'SS', 'KK', 'Associate', 'ssn100', 'R605166');
-insert into USER (ID, EMAIL, FIRST_NAME, LAST_NAME, ROLE, SSN, USER_NAME) values(105, 'yy@gmail.com', 'YY', 'KK', 'Developer', 'ssn104', 'R705616');
-
-
+insert into USER (ID, EMAIL, FIRST_NAME, LAST_NAME, ROLE, SSN, USER_NAME) values(101, 'abc@gmail.com', 'LL', 'KK', 'Associate', 'ssn10', '668403');
+insert into USER (ID, EMAIL, FIRST_NAME, LAST_NAME, ROLE, SSN, USER_NAME) values(102, 'fcd@gmail.com', 'KK', 'KK', 'VP', 'ssn101', '66736');
+insert into USER (ID, EMAIL, FIRST_NAME, LAST_NAME, ROLE, SSN, USER_NAME) values(103, 'sku@gmail.com', 'DD', 'KK', 'Junior Associate', 'ssn110', '342432');
+insert into USER (ID, EMAIL, FIRST_NAME, LAST_NAME, ROLE, SSN, USER_NAME) values(104, 'fff@gmail.com', 'SS', 'KK', 'Associate', 'ssn100', '4324242');
+insert into USER (ID, EMAIL, FIRST_NAME, LAST_NAME, ROLE, SSN, USER_NAME) values(105, 'yy@gmail.com', 'YY', 'KK', 'Developer', 'ssn104', '3243423');
